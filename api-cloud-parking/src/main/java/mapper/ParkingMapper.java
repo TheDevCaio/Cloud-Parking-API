9d87485fef25234327a17b5a7374c77e7379cb43
Caio Dev <110746976+TheDevCaio@users.noreply.github.com>
2022-11-24 15:04:41 -0300

    pom.xml
    Arquivos POM.XML do projeto.

---
+++ api-cloud-parking/src/main/java/mapper/ParkingMapper.java
@@ -0,0 +1,33 @@
+package mapper;
+
+import dto.ParkingCreateDTO;
+import dto.ParkingDTO;
+import model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO toparkingDTO(Parking parking){
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toparkingDTOList(List<Parking> parkingList){
+        return parkingList.stream().map(this::toparkingDTO).collect(Collectors.toList());
+    }
+
+    public Parking toparking(ParkingDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+
+    public Parking toparkingCreate(ParkingCreateDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+
+}
